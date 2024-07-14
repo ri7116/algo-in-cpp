226163f7356bf97904ba094ad2afe0179901b331
Codekookie <121166835+Codekookie@users.noreply.github.com>
2024-07-15 01:29:43 +0900

    [Silver II] Title: 과자 나눠주기, Time: 180 ms, Memory: 5928 KB -BaekjoonHub

---
+++ C++17/백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <cstdio>
+
+using namespace std;
+
+int snack[1000001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int M, N;
+
+	int start = 1;
+	int end = 0;
+	int ans = 0;
+
+	cin >> M >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> snack[i];
+	}
+
+	end = *max_element(snack, snack+N);
+
+	while (start <= end)
+	{
+		int cum_sum = 0;
+		int mid = (start + end) / 2;
+
+		for (int i = 0; i < N; i++)
+		{
+			cum_sum += snack[i] / mid;
+		}
+
+		if (cum_sum >= M)
+		{
+			ans = mid;
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+	
+	if (ans == 0) cout << "0";
+	else cout << ans;
+}
