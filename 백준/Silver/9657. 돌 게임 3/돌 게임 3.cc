e350ad1bba3262e9be870f01c1f36ca430a15acb
yoochanhyck <121166835+ri7116@users.noreply.github.com>
2025-01-29 23:06:35 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+string d[1004];
+int main(){
+  cin>>n;
+  d[1]="SK";//""SK" ==잡은사람이 이김" "2는 후턴"
+  d[2]="CY";//후턴이
+  d[3]="SK";//잡은사람이
+  d[4]="SK"; // 3 "SK" / "SK" 3 / "SK" 1 1 1
+  for(int i=5;i<=1000;i++){
+    if(d[i-1]== "CY" or d[i-3]== "CY" or d[i-4]== "CY" ) d[i]="SK";
+    else d[i]="CY"; 
+  }
+  cout<<d[n];
+}
