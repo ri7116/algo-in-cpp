abb63725b1e1f500e67a9b56cb1c461b801c895e
Yoo <121166835+ri7116@users.noreply.github.com>
2025-02-24 21:16:07 +0900

    [Silver V] Title: 번데기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15721. 번데기/번데기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, t, want, n = 0, cnt = 0, res = 0;
+	cin >> a >> t >> want;
+	while (true) {
+		n++; //라운드
+		vector<int> what{ 0,1,0,1 }; //0뻔 1데기
+		for (int i = 1; i <= n + 1; i++) what.push_back(0); //뻔 추가
+		for (int i = 1; i <= n + 1; i++) what.push_back(1); //데기 추가
+		for (int i = 0; i < what.size(); i++) {
+			if (what[i] == want) cnt++; //등장 횟수 카운트
+			if (cnt == t) {
+				cout << res;
+				return 0;
+			}
+			res++; //몇번째 사람인지 센다
+			res %= a;
+		}
+		what.clear();
+	}
+}
