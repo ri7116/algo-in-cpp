452dcda610e0733106d00283acd1a86ad3dd7b82
Yoo <121166835+ri7116@users.noreply.github.com>
2025-02-28 15:52:19 +0900

    [Silver III] Title: iSharp, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Silver/3568. iSharp/iSharp.cc
+++ C++17/백준/Silver/3568. iSharp/iSharp.cc
@@ -1,59 +1,79 @@
-#include<bits/stdc++.h>
-using namespace std;
-string start="";
-string a;
-vector<string> tmp[100];
-int cnt;
-int main(){
-    getline(cin, a);  // 전체 라인을 읽어들임
-    //cout<<"a: "<<a<<endl;
-    cnt=0;
-    //cout<<"asize: "<<a.size()<<endl;
-    for(int i=0;i<a.size()-1;i++){
-        if(a[i]==' ') 
-        {
-            cnt++;//공백을 어떻게 확인?
-    //        cout<<"gong back"<<endl;
-            continue;
-        }
-        if(cnt==0) {
-            start.push_back(a[i]);//처음 글자는 start에 저장
-    //        cout<<"start in"<<endl;
-        }
-        else {
-            tmp[cnt-1].push_back(string(1,a[i]));//두번째부터는 tmp에 저장하기
-    //        cout<<"tmp in"<<endl;
-        }
-    }
-    //cout<<"cnt :"<<cnt<<endl;
+#include <iostream>
+#include <vector>
+#include <string>
 
-    for(int i=0;i<cnt;i++){
-        cout<<start;
-        bool check=0;
-        int lastidx=0;
-        for(int j=tmp[i].size()-1;j>=0;j--){
-            if(tmp[i][j]==",") continue;
-            if("A"<=tmp[i][j] and tmp[i][j]<="Z" and check==0){
-                cout<<" ";
-                //<<tmp[i][j];
-                lastidx=j;
-                check=1;
-                break;
-            }
-            else if("a"<=tmp[i][j] and tmp[i][j]<="z" and check ==0){
-                cout<<" ";
-                //<<tmp[i][j];
-                lastidx=j;
-                check=1;
-                break;
-            }
-            else if(tmp[i][j]=="[") cout<<"]";
-            else if(tmp[i][j]=="]") cout<<"[";
-            else cout<<tmp[i][j];
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    getline(cin, s);
+
+    string init;
+    vector<string> v;
+    string temp;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {  // 공통된 변수형
+            init = temp;
+            temp.clear();
+            continue;
+        } 
+        else if (s[i] == ',') {  // 하나의 추가 변수형 + 변수 이름의 끝
+            v.push_back(temp);
+            i++;  // 다음 문자로 이동
+            temp.clear();
+        } 
+        else if (s[i] == ';') {  // 끝
+            v.push_back(temp);
+            temp.clear();
+        } 
+        else {
+            temp += s[i];
         }
-        for(int j=0;j<=lastidx;j++) cout<<tmp[i][j];
-        cout<<";"<<endl;
     }
+
+    for (int i = 0; i < v.size(); i++) {
+        int idx = -1;
+
+        // 추가적인 변수형 유무 파악
+        for (int j = 0; j < v[i].size(); j++) {
+            if (!((v[i][j] >= 'a' && v[i][j] <= 'z') || (v[i][j] >= 'A' && v[i][j] <= 'Z'))) {
+                idx = j;
+                break;
+            }
+        }
+
+        cout << init;
+
+        // 추가적인 변수형이 없는 경우
+        if (idx == -1) {
+            cout << " " << v[i];
+        } 
+        // 추가적인 변수형이 있는 경우
+        else {
+            // 추가적인 변수형 역순으로 출력
+            for (int j = v[i].size() - 1; j >= idx; j--) {
+                if (v[i][j] == ']') {
+                    cout << "[]";
+                    j--;  // 대괄호 맞추기
+                    continue;
+                }
+                cout << v[i][j];
+            }
+
+            cout << " ";
+
+            // 변수 이름 출력
+            for (int j = 0; j < idx; j++) {
+                cout << v[i][j];
+            }
+        }
+
+        cout << ";\n";
+    }
+
+    return 0;
 }
-//vector<string> tmp[100];
-//
