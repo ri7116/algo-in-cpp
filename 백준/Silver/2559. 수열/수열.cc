52935e79db7a7b3b656146216958f6cbddca1c70
Yoo <121166835+ri7116@users.noreply.github.com>
2025-03-15 21:10:37 +0900

    [Silver III] Title: 수열, Time: 36 ms, Memory: 80144 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> //누적합!
+using namespace std;
+int N, K, temp, MAX;
+int arr[10000001], sum[10000001];
+int main(){
+cin>>N >> K;
+for(int i=1; i<=N;i++){//배열에 입력받기 
+    cin>>arr[i];
+    temp+=arr[i];
+    sum[i]+=temp;
+}
+
+MAX=sum[K];
+
+for(int i=K+1;i<=N;i++){
+    if(sum[i]-sum[i-K]>=MAX) MAX=sum[i]-sum[i-K];
+}
+
+cout<< MAX;
+}
