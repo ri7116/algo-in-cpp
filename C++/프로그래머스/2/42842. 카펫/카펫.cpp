5358876ecea3d35d4e603e9838a5a418aa819f55
Yoo <121166835+ri7116@users.noreply.github.com>
2025-03-20 22:01:21 +0900

    [level 2] Title: 카펫, Time: 27.08 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/42842. 카펫/카펫.cpp
+++ C++/프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int n,m;
 vector<int> solution(int brown, int yellow) {
     for(int i=0;i<2500;i++){
-        for(int j=0;j<1000000;j++){
+        for(int j=0;j<10000;j++){
             if(yellow==(i-2)*(j-2) and brown==2*i+(j-2)*2) {
                 m=i;
                 n=j;
