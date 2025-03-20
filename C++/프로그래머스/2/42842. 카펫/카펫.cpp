b526175522eff91e0a0787a6a8e8c9c43c3694e5
Yoo <121166835+ri7116@users.noreply.github.com>
2025-03-20 21:55:12 +0900

    [level 2] Title: 카펫, Time: 1959.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int n,m;
+vector<int> solution(int brown, int yellow) {
+    for(int i=0;i<2500;i++){
+        for(int j=0;j<1000000;j++){
+            if(yellow==(i-2)*(j-2) and brown==2*i+(j-2)*2) {
+                m=i;
+                n=j;
+            }
+        }
+    }
+    
+    vector<int> answer;
+    answer.push_back(m);
+    answer.push_back(n);
+    return answer;
+}
