347d941d21d910786e8a1023dd453a055208d4a9
Yoo <121166835+ri7116@users.noreply.github.com>
2025-06-19 14:39:09 +0900

    [Gold IV] Title: ⚾, Time: 492 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Gold/17281. ⚾/⚾.cc
+++ C++17/백준/Gold/17281. ⚾/⚾.cc
@@ -10,12 +10,6 @@ void dfs(int depth){
     if(depth==10){
         int tmp=0;
         taza=0;
-        
-        // for(int i=1;i<=9;i++){
-        //     cout<<Map[i][1]<<" ";
-        // }
-        // cout<<endl;
-
         for(int i=1;i<=n;i++){//이닝
             int cnt=0;
             //루 초기화
@@ -23,57 +17,48 @@ void dfs(int depth){
                 Map[j][2]=0;
             }
             //이닝 시작 알림
-            
-            // cout<<"ening start \n";
+            //cout<<"ening start \n";
 
 
             while(1){
                 taza++;
                 if(taza>=10) taza=taza-9;
-                
                 //cout<<taza<<" gogogogo\n";
-                
-                if(ening_map[i][Map[taza][1]]==1){ // 1루타
-                    // 3루에 있던 주자는 득점
-                    for(int k=1; k<=9; k++) if(Map[k][2] == 3) { Map[k][2] = 4; }
-                    // 2루에 있던 주자는 3루로
-                    for(int k=1; k<=9; k++) if(Map[k][2] == 2) { Map[k][2] = 3; }
-                    // 1루에 있던 주자는 2루로
-                    for(int k=1; k<=9; k++) if(Map[k][2] == 1) { Map[k][2] = 2; }
-                    
-                    // 타자는 1루로
-                    Map[taza][2] = 1;
-
-                    // 점수 계산 및 베이스 정리
-                    for(int k=1; k<=9; k++){
-                        if(Map[k][2] >= 4) {
-                            tmp++;
-                            Map[k][2] = 0;
+                if(ening_map[i][Map[taza][1]]==1){//i번째 이닝의 taza의 선수 번호 
+                    for(int k=1;k<=9;k++){//1번타자 부터 흝고
+                        if(Map[k][2]!=0){// 경기장에 있으면
+                            Map[k][2]=Map[k][2]+1;
+                            if((Map[k][2])>=4){
+                                Map[k][2]=0;
+                                tmp++;
+                            }
                         }
                     }
+                    Map[taza][2]=1;
                 }
-                if(ening_map[i][Map[taza][1]]==2){ // 2루타
-                    // 3루, 2루 주자는 득점
-                    for(int k=1; k<=9; k++) if(Map[k][2] == 3 || Map[k][2] == 2) { 
-                        tmp++; 
-                        Map[k][2] = 0; // 즉시 베이스에서 제거
-                    }
-                    // 1루 주자는 3루로
-                    for(int k=1; k<=9; k++) if(Map[k][2] == 1) { Map[k][2] = 3; }
-
-                    // 타자는 2루로
-                    Map[taza][2] = 2;
-                }
-                if(ening_map[i][Map[taza][1]]==3){ // 3루타
-                    // 모든 주자는 득점
-                    for(int k=1; k<=9; k++) {
-                        if(Map[k][2] > 0) {
-                            tmp++;
-                            Map[k][2] = 0;
+                if(ening_map[i][Map[taza][1]]==2){
+                    for(int k=1;k<=9;k++){//1번타자 부터 흝고
+                        if(Map[k][2]!=0 ){// 경기장에 있으면
+                            Map[k][2]=Map[k][2]+2;
+                            if((Map[k][2])>=4){
+                                Map[k][2]=0;
+                                tmp++;
+                            }
                         }
                     }
-                    // 타자는 3루로
-                    Map[taza][2] = 3;
+                    Map[taza][2]=2;
+                }
+                if(ening_map[i][Map[taza][1]]==3){
+                    for(int k=1;k<=9;k++){//1번타자 부터 흝고
+                        if(Map[k][2]!=0){// 경기장에 있으면
+                            Map[k][2]=Map[k][2]+3;
+                            if((Map[k][2])>=4){
+                                Map[k][2]=0;
+                                tmp++;
+                            }
+                        }
+                    }
+                    Map[taza][2]=3;
                 }
                 if(ening_map[i][Map[taza][1]]==4){
                     for(int k=1;k<=9;k++){//1번타자 부터 흝고
