3fffd45fb835a4d2dab68bad1145de42cd7a5acc
Yoo <121166835+ri7116@users.noreply.github.com>
2025-06-20 22:48:00 +0900

    [Silver IV] Title: 대칭 차집합, Time: 220 ms, Memory: 5108 KB -BaekjoonHub

--- C++17/백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ C++17/백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,28 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,m;
-set<int> s1;
-set<int> s2;
+vector<int> v1;
+vector<int> v2;
 int a;
+
+int check(int target){
+	int st=0;
+	int ed=m-1;
+	int mid;
+	while(st<=ed){
+		mid=(st+ed)/2;
+
+		if(v2[mid]==target) return 1;
+		else if(v2[mid]<target) st=mid+1;
+		else ed=mid-1; //(v2[mid]<target)
+	}
+	return 0;
+}
+
+
 int main(){
 	cin>>n>>m;
 	for(int i=0;i<n;i++){
 		cin>>a;	
-		s1.insert(a);
+		v1.push_back(a);
 	}
 	for(int i=0;i<m;i++){
-		cin>>a;	
-		s2.insert(a);
+		cin>>a;
+		v2.push_back(a);
 	}
-	int cnt=0;
-	for( auto a:s1){
-		if(s2.find(a)!=s2.end()) cnt++;
-	}
+	
+	sort(v1.begin(),v1.end());
+	sort(v2.begin(),v2.end());
+	
 	int ans=0;
-	ans=ans+s1.size()-cnt;
-	ans=ans+s2.size()-cnt;
+	int cnt=0;
+	for(auto k: v1){
+		if(check(k)) cnt++;
+	}
+	ans+=v1.size()-cnt;
+	ans+=v2.size()-cnt;
+
 	cout<<ans;
 	
 }
-//1. auto set은 하나씩 접근가능
-//2. cout<<s1[1];은 불가능
+//이분 탐색을 이용한 풀이, set을 이용한 풀이 두 가지가 존재함
