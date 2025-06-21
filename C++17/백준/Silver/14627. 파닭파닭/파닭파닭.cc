f13c219c0e91a2bf9b880352c92e9218fb33cec7
Yoo <121166835+ri7116@users.noreply.github.com>
2025-06-22 00:37:13 +0900

    [Silver II] Title: 파닭파닭, Time: 240 ms, Memory: 17644 KB -BaekjoonHub

--- C++17/백준/Silver/14627. 파닭파닭/파닭파닭.cc
+++ C++17/백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -1,49 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-long long arr[1000004]; // 파 길이 저장 배열
-long long n, m;         // 파 개수, 사람 수
+long long t;
+long long arr[1000004];
+long long arr2[1000004];
+long long n,m, mx;
+long long ans=1e18;
+long long check(long long target){
+	if(target>mx) return 0;
+	long long cnt=0;
+	for(long long i=0;i<n;i++){
+		cnt+=(arr[i]/target);
+	}
 
-// mid 길이로 잘랐을 때 몇 조각 나오는지 계산
-long long check(long long target) {
-    long long cnt = 0;
-    for (int i = 0; i < n; i++) {
-        cnt += arr[i] / target;
-    }
-    return cnt;
+	return cnt;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin>>n>>m;//파 닭
+	long long Sum=0;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+		Sum+=arr[i];
+		mx=max(arr[i],mx);
+	}
 
-    cin >> n >> m;
-    long long sum = 0;
-    long long mx = 0;
+	long long st=1;
+	long long ed=1e18;
+	long long mid;
+	while(st<=ed){
+		mid=(st+ed)/2;
 
-    // 입력 받으며 전체 길이와 최대 길이 계산
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        sum += arr[i];
-        mx = max(mx, arr[i]);
-    }
-
-    long long st = 1;
-    long long ed = mx;
-    long long res = 0;
-
-    // 가능한 최대 파 길이 이진 탐색
-    while (st <= ed) {
-        long long mid = (st + ed) / 2;
-        if (check(mid) >= m) {
-            res = mid;        // 가능한 최대 길이 저장
-            st = mid + 1;     // 더 긴 길이 탐색
-        } else {
-            ed = mid - 1;     // 더 짧은 길이 탐색
-        }
-    }
-
-    // 최종적으로 남는 파의 양 출력
-    cout << sum - res * m << '\n';
-    return 0;
+		if(check(mid)>=m){
+			st=mid+1;
+//			ans=min(ans,Sum-mid*check(mid));
+			ans=min(ans,Sum-mid*m);
+		}
+		else{
+			ed=mid-1;
+		}
+	}
+	cout<<ans;
 }
