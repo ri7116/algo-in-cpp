fedfdbecfeb529e5df0698abe72d534de7b3ade2
Yoo <121166835+ri7116@users.noreply.github.com>
2025-06-23 21:47:08 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 0 ms, Memory: 3584 KB -BaekjoonHub

--- C++17/백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ C++17/백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -1,24 +1,22 @@
-//1. .4f
-//2. long long은 int임
+//두번째 풀이 그리디
+//현재와 이전을 곱하고 곱한 값이 더 작으면 현재값 그대로 적기 아니면 곱한 값 적기 
 #include<bits/stdc++.h>
 using namespace std;
 int n;
 double arr[100004];
-double ans=-1;
+double ans[100004];
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(0);
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>arr[i];
-        ans=max(ans,arr[i]);
     }
-    for(int i=0;i<n-1;i++){
-        double tmp=arr[i];
-        for(int j=i+1;j<n;j++){
-            tmp*=arr[j];
-            ans=max(ans,tmp);
-        }
+    ans[0]=arr[0];
+    for(int i=1;i<n;i++){
+        if(ans[i-1]*arr[i]>arr[i]) ans[i]=ans[i-1]*arr[i];
+        else ans[i]=arr[i];
     }
-    printf("%.3f\n", ans);
+    sort(ans,ans+n);
+    printf("%.3f",ans[n-1]);
 }
