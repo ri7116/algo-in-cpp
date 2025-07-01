ce55e4e19fe0f667f4965da5986d2b5826a6ea5e
Yoo <121166835+ri7116@users.noreply.github.com>
2025-07-02 00:50:28 +0900

    [Gold IV] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- C++17/백준/Gold/2240. 자두나무/자두나무.cc
+++ C++17/백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,66 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int t;
-int w;
-int dp[1004][44][44];
-int arr[1004];
-int INF=-987654321;
-int ans=-987654321;
+int dp[1004][2][34], n, m, b[1004];
 
-int solve(int t,int position, int w){
-    //기저사례
-    if(t<=0) return 0;
-    if(w<=0) return 0;
-    if(t==1){
-        if(position==1 && arr[1]==1 && w>=1) return 1;  // 1번에서 시작, 1번에서 떨어짐, 이동 없음
-        if(position==2 && arr[1]==2 && w>=2) return 1;  // 2번으로 이동, 2번에서 떨어짐, 1번 이동
-        return 0;
-    }
-    //계산되어 있으면 넘어가
-    if(dp[t][position][w]!=-1) return dp[t][position][w];
-    //계산해
-    //떨어지는 위치
-    // dp[t][1][w]= max(w(t-1,2,w-1)+1, w(t-1,1,w)+1);
-    // dp[t][2][w]= max(w(t-1,1,w-1)+1, w(t-1,2,w)+1);
-    // return max(dp[t][1][w],dp[t][2][w])
-    if(arr[t]==1){
-        // 1번에서 자두가 떨어짐
-        if(position==1){
-            // 1번 위치에 있음 - 받을 수 있음
-            return dp[t][1][w]= max(solve(t-1,1,w), solve(t-1,2,w-1)) + 1;
-        }
-        else{
-            // 2번 위치에 있음 - 받을 수 없음
-            return dp[t][2][w]= max(solve(t-1,1,w-1), solve(t-1,2,w));
-        }
-    }
-    else{
-        // 2번에서 자두가 떨어짐
-        if(position==1){
-            // 1번 위치에 있음 - 받을 수 없음
-            return dp[t][1][w]= max(solve(t-1,1,w), solve(t-1,2,w-1));
-        }
-        else{
-            // 2번 위치에 있음 - 받을 수 있음
-            return dp[t][2][w]= max(solve(t-1,1,w-1), solve(t-1,2,w)) + 1;
-        }
-    }
+int go (int idx, int tree, int cnt){
+    //cout<<"go ("<< idx <<" "<<tree<<" "<<cnt<<")"<<endl;
+    if(cnt<0) return -1e9;
+    if(idx==n) return 0;
+
+    if(~dp[idx][tree][cnt]) return dp[idx][tree][cnt];
+    return dp[idx][tree][cnt]= max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
 }
 
 
 int main(){
-    memset(dp, -1, sizeof(dp));
-    cin >> t >> w;
-    for(int i=1;i<=t;i++){
-        cin>>arr[i];
-    }
-    
-    // 모든 이동 횟수에서 최대값 찾기 (1부터 w+1까지 = 실제 0부터 w번 이동)
-    for(int moves=1; moves<=w+1; moves++){
-        ans = max(ans, solve(t, 1, moves));
-        ans = max(ans, solve(t, 2, moves));
-    }
-    
-    cout << ans << endl;
+    memset(dp,-1,sizeof(dp));
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> b[i]; 
+    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n'; 
     return 0;
 }
+
+//미래지향 dp
