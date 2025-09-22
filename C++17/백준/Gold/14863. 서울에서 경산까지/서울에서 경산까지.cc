ea83ae062544fa2c0ed85069b937c3a433844142
Yoo <121166835+ri7116@users.noreply.github.com>
2025-09-23 04:29:39 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 60 ms, Memory: 83272 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
+++ C++17/백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
@@ -1,37 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-int k;
-int dp[104][100004];
-int a,b,c,d;
+long long n;
+long long k;
+long long dp[104][100004];
+long long a,b,c,d;
 
 int main(){
     cin>>n>>k;
     for(int i=1;i<=n;i++){
+        for(int j=0;j<=k;j++){
+            dp[i][j]=-1;
+        }
+    }
+    for(int i=1;i<=n;i++){
         cin>>a>>b>>c>>d; // 시간 금액 시간 금액
         if(i==1){
-            dp[1][a]=b;
-            dp[1][c]=d;
+            dp[1][a]=max(dp[1][a],b);
+            dp[1][c]=max(dp[1][c],d);
         }
         else{
             for(int j=1;j<=k;j++){
-                if(dp[i-1][j]!=0){
-                    dp[i][j+a]=max(dp[i][j+a],dp[i-1][j]+b);
-                    dp[i][j+c]=max(dp[i][j+c],dp[i-1][j]+d);
+                if(dp[i-1][j]!=-1){
+                    if(j+a<=k) dp[i][j+a]=max(dp[i][j+a],dp[i-1][j]+b);
+                    if(j+c<=k) dp[i][j+c]=max(dp[i][j+c],dp[i-1][j]+d);
                 }
             }
         }
     }
-    
-    int ans=0;
+
+    long long ans=0;
     for(int j=1;j<=k;j++){
     //    cout<<dp[n][j]<<endl;
-        if(dp[n][j]!=0){
+        if(dp[n][j]!=-1){
             ans=max(ans, dp[n][j]);
         }
     }
     cout<<ans;
 }
-
-//1. DP
-//2. N * K = 100 * 100000 = 10000000  => 천만이므로 계산이 무조건 가능함.
